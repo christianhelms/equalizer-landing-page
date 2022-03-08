8c7d8c7628a58a4e342015fbf7fa39a1b0a9f88b
christianhelms <helmsmedia@gmail.com>
2022-03-08 17:53:25 -0600

    added background design one and fixed padding and size

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -9,17 +8,29 @@ export default function Home() {
         <title>Equalizer Landing Page</title>
       </Head>
       <body className='w-auto h-auto pt-5 pr-20 pl-20 tb-5' id='main'>
-        <div className=''>
-          <p className='text-ibm-plex-sans font-bold text-4xl'>equalizer</p>
-          <h1 className='text-ibm-plex-sans font-bold text-7xl'>We make your music sound extraordinary.</h1>
-          <p>A system audio equalizer specifically designed for Android and iOS. 
-            Freely tune the way your music sounds with a professional grade parametric EQ & volume mixer. 
-            Control bass, mids, treble, gain control, reverb, and more! Premium EQ Get expert-level control with a robust equalizer, volume mixer, and spatial audio. 
-            Take your listening experience to a whole new level and access all our incredible features! 
-            $4 / month iOS Download Android Download All rights reserved © Equalizer 2021 Have any problems? 
-            Contact us via social media or email us at equalizer@example.com</p>
+        <div className='flex'>
+          <div className='pr-14'>
+          <p className='text-ibm-plex-sans font-bold text-4xl pb-16'>equalizer</p>
+            <h1 className='text-ibm-plex-sans font-bold text-7xl pb-10'>We make your music sound extraordinary.</h1>
+            <p className='text-ibm-plex-sans font-normal text-base'>A system audio equalizer specifically designed for Android and iOS. 
+              Freely tune the way your music sounds with a professional grade parametric EQ & volume mixer. 
+              Control bass, mids, treble, gain control, reverb, and more!</p>
+          </div>
+          <div>
+          <img 
+          src="/bg-pattern-1.svg" 
+          alt="Background pattern"
+          width={600}
+          height={900}
+          />
+          </div>
         </div>
       </body>
     </main>
   )
 }
+
+// Premium EQ Get expert-level control with a robust equalizer, volume mixer, and spatial audio. 
+// Take your listening experience to a whole new level and access all our incredible features! 
+// $4 / month iOS Download Android Download All rights reserved © Equalizer 2021 Have any problems? 
+// Contact us via social media or email us at equalizer@example.com
